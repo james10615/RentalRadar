9888db3de208ffa8e0cc93afd647dc4ddeab20d4
James Simanto <jamessimanto@gmail.com>
2024-08-22 05:06:13 +0300

    Create db_connect.php

---
+++ db_connect.php
@@ -0,0 +1,9 @@
+<?php
+
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "rentalradar";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
