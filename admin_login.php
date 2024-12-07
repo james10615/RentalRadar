6b46e040dd132698ddadf619a7adc695193f509c
James Simanto <jamessimanto@gmail.com>
2024-12-07 04:25:18 +0300

    Update admin_login.php

--- admin_login.php
+++ admin_login.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 
+if (isset($_SESSION['message'])) {
+    echo "<div class='alert alert-success'>" . $_SESSION['message'] . "</div>";
+    unset($_SESSION['message']);
+}
+
 // Database connection
 $servername = "localhost";
 $username = "root";
